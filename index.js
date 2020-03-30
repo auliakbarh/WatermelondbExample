0ca6614dddc565b164cd38eed10a2e9c44fb0e0f
auliakbarh <auliaakbarharahaap@gmail.com>
2020-03-30 17:01:51 +0700

    setup database in index.js

--- index.js
+++ index.js
@@ -3,8 +3,32 @@
  */
 
 import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
-import App from './App';
+import {AppRegistry, NativeModules} from 'react-native';
 import {name as appName} from './app.json';
+import {Database} from '@nozbe/watermelondb';
+import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite';
 
-AppRegistry.registerComponent(appName, () => App);
+import {mySchema} from './src/database/schema';
+import Blog from './src/database/models/Blog';
+import Post from './src/database/models/Post';
+import Comment from './src/database/models/Comment';
+
+import {createNavigation} from './src/router';
+
+const adapter = new SQLiteAdapter({
+  dbName: 'WatermelonDemo',
+  schema: mySchema,
+});
+
+const database = new Database({
+  adapter,
+  modelClasses: [Blog, Post, Comment],
+  actionsEnabled: true,
+});
+
+const appStartedLaunchingAt = NativeModules.PerformancePlugin.appInitTimestamp;
+const timeToLaunch = new Date().getTime() - appStartedLaunchingAt;
+
+const Navigation = createNavigation({database, timeToLaunch});
+
+AppRegistry.registerComponent(appName, () => Navigation);
